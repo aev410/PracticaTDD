a169732fe91c457928432cc1c788bf19a0e460a1
Antonio <aev0004@alu.medac.es>
2023-02-14 16:50:44 +0100

    inicial comit

---
+++ calc.py
@@ -0,0 +1,23 @@
+class Calc:
+    a = 0
+    b = 0
+    def __init__(Self,a,b):
+        Self.a = a
+        Self.b = b
+    
+    def suma(Self):
+        return Self.a+Self.b
+
+    def resta(Self):
+        return Self.a-Self.b
+    
+    def prod(Self):
+        return Self.a*Self.b
+    
+    def div(Self):
+        if Self.b == 0.0:
+            raise ZeroDivisionError
+        return Self.a/Self.b
+    
+    def mod(Self):
+        return Self.a%Self.b
